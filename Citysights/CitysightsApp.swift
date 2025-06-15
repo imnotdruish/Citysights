9863821fcb4f7386af36c21dec02f4be1348a15d
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-06-15 15:34:21 -0400

    Initial Commit

---
+++ Citysights/CitysightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitysightsApp.swift
+//  Citysights
+//
+//  Created by Dan June on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CitysightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
